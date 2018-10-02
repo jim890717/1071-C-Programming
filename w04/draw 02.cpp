3bd9982d5cea02ff0c57a84d3d4f796fbfbe7437
jim890717 <43335027+jim890717@users.noreply.github.com>
2018-10-02 17:56:10 +0800

    add draw.cpp

---
+++ w04/draw 02.cpp
@@ -0,0 +1,257 @@
+#include <stdio.h>
+
+void prachar(char c,int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        printf("%c",c);
+    }
+}
+
+void rectan(int height,int width,int filled)
+{
+
+    if(filled)
+    {
+        for(int i=1; i<=height; i++)
+        {
+            for(int j=1; j<=width; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        for(int i=1; i<=height; i++)
+        {
+            for(int j=1; j<=width; j++)
+            {
+                if(i==1 || i==height || j==1 || j==width)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+
+            }
+            printf("\n");
+        }
+
+    }
+
+
+
+}
+
+void drawvertri(int height,int type,int filled)
+{
+    switch(type)
+    {
+    case 1:
+
+        if(filled)
+        {
+            for(int i=1; i<=height; i++)
+            {
+                prachar('*',i);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+
+                if(i==1||i==height)
+                {
+                    prachar('*',i);
+                }
+                else
+                {
+                    printf("*");
+                    prachar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
+
+        }
+        break;
+    case 2:
+        if(filled)
+        {
+            for(int i=height; i>=1; i--)
+            {
+                prachar('*',i);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=height; i>=1; i--)
+            {
+
+                if(i==1||i==height)
+                {
+                    prachar('*',i);
+                }
+                else
+                {
+                    printf("*");
+                    prachar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
+
+        }
+        break;
+    case 3:
+        if(filled)
+        {
+            for(int i=1; i<=height; i++)
+            {
+                prachar(' ',5-i);
+                prachar('*',i);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+
+                if(i==1||i==height)
+                {
+                    prachar(' ',5-i);
+                    prachar('*',i);
+                }
+                else
+                {
+                    prachar(' ',5-i);
+                    printf("*");
+                    prachar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
+
+        }
+
+
+
+        break;
+    case 4:
+        if(filled)
+        {
+            for(int i=height; i>=1; i--)
+            {
+                prachar(' ',5-i);
+                prachar('*',i);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=height; i>=1; i--)
+            {
+
+                if(i==1||i==height)
+                {
+                    prachar(' ',5-i);
+                    prachar('*',i);
+                }
+                else
+                {
+                    prachar(' ',5-i);
+                    printf("*");
+                    prachar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
+
+        }
+        break;
+
+    }
+
+}
+
+void tsoscele(int height,int type,int filled)
+{
+
+    switch(type)
+    {
+    case 1:
+        if(filled){
+                for(int=1;i<=height;i++){
+                    
+                    
+                    
+                }
+            
+        }else{
+        
+        }
+
+        break;
+    case 2:
+        break;
+
+
+
+    }
+
+}
+
+int main()
+{
+
+    int choice,height,width,type,filled;
+
+
+    while(1)
+    {
+        printf("Main Menu\n");
+        printf("1. Rectangle\n");
+        printf("2. Vertical Triangle\n");
+        printf("3.Tsosceles Triangle\n");
+        printf("4. Exit\n");
+        printf("=>");
+        scanf("%d",&choice);
+        if(choice==4)break;
+
+        switch(choice)
+        {
+
+        case 1:
+            printf("Enter height and width and filled:");
+            scanf("%d %d %d",&height,&width,&filled);
+            rectan(height,width,filled);
+
+
+            break;
+
+        case 2:
+            printf("Enter height and type and filled:");
+            scanf("%d %d %d",&height,&type,&filled);
+            drawvertri(height,type,filled);
+
+        }
+        break;
+    case 3:
+        printf("Enter height and type and filled:");
+        scanf("%d %d %d",&height,&type,&filled);
+        tsoscele(hieght,type,filled);
+    }
+
+
+
+
+    return 0;
+}
